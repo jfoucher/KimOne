c59a9600e3cc4a6390104389e0ffd9828ac218cc
Jonathan Foucher <jfoucher@gmail.com>
2021-07-24 12:33:33 +0200

    Add license comments to source files

--- KimOne/cpu.h
+++ KimOne/cpu.h
@@ -3,8 +3,116 @@
 //  Cesium
 //
 //  Created by Jonathan Foucher on 15/07/2021.
-//  Copyright © 2021 Jonathan Foucher. All rights reserved.
-//
+
+/* Fake6502 CPU emulator core v1.1 *******************
+ * (c)2011 Mike Chambers (miker00lz@gmail.com)       *
+ *****************************************************
+ * v1.1 - Small bugfix in BIT opcode, but it was the *
+ *        difference between a few games in my NES   *
+ *        emulator working and being broken!         *
+ *        I went through the rest carefully again    *
+ *        after fixing it just to make sure I didn't *
+ *        have any other typos! (Dec. 17, 2011)      *
+ *                                                   *
+ * v1.0 - First release (Nov. 24, 2011)              *
+ *****************************************************
+ * LICENSE: This source code is released into the    *
+ * public domain, but if you use it please do give   *
+ * credit. I put a lot of effort into writing this!  *
+ *                                                   *
+ *****************************************************
+ * Fake6502 is a MOS Technology 6502 CPU emulation   *
+ * engine in C. It was written as part of a Nintendo *
+ * Entertainment System emulator I've been writing.  *
+ *                                                   *
+ * A couple important things to know about are two   *
+ * defines in the code. One is "UNDOCUMENTED" which, *
+ * when defined, allows Fake6502 to compile with     *
+ * full support for the more predictable             *
+ * undocumented instructions of the 6502. If it is   *
+ * undefined, undocumented opcodes just act as NOPs. *
+ *                                                   *
+ * The other define is "NES_CPU", which causes the   *
+ * code to compile without support for binary-coded  *
+ * decimal (BCD) support for the ADC and SBC         *
+ * opcodes. The Ricoh 2A03 CPU in the NES does not   *
+ * support BCD, but is otherwise identical to the    *
+ * standard MOS 6502. (Note that this define is      *
+ * enabled in this file if you haven't changed it    *
+ * yourself. If you're not emulating a NES, you      *
+ * should comment it out.)                           *
+ *                                                   *
+ * If you do discover an error in timing accuracy,   *
+ * or operation in general please e-mail me at the   *
+ * address above so that I can fix it. Thank you!    *
+ *                                                   *
+ *****************************************************
+ * Usage:                                            *
+ *                                                   *
+ * Fake6502 requires you to provide two external     *
+ * functions:                                        *
+ *                                                   *
+ * uint8_t read6502(uint16_t address)                *
+ * void write6502(uint16_t address, uint8_t value)   *
+ *                                                   *
+ * You may optionally pass Fake6502 the pointer to a *
+ * function which you want to be called after every  *
+ * emulated instruction. This function should be a   *
+ * void with no parameters expected to be passed to  *
+ * it.                                               *
+ *                                                   *
+ * This can be very useful. For example, in a NES    *
+ * emulator, you check the number of clock ticks     *
+ * that have passed so you can know when to handle   *
+ * APU events.                                       *
+ *                                                   *
+ * To pass Fake6502 this pointer, use the            *
+ * hookexternal(void *funcptr) function provided.    *
+ *                                                   *
+ * To disable the hook later, pass NULL to it.       *
+ *****************************************************
+ * Useful functions in this emulator:                *
+ *                                                   *
+ * void reset6502()                                  *
+ *   - Call this once before you begin execution.    *
+ *                                                   *
+ * void exec6502(uint32_t tickcount)                 *
+ *   - Execute 6502 code up to the next specified    *
+ *     count of clock ticks.                         *
+ *                                                   *
+ * void step6502()                                   *
+ *   - Execute a single instrution.                  *
+ *                                                   *
+ * void irq6502()                                    *
+ *   - Trigger a hardware IRQ in the 6502 core.      *
+ *                                                   *
+ * void nmi6502()                                    *
+ *   - Trigger an NMI in the 6502 core.              *
+ *                                                   *
+ * void hookexternal(void *funcptr)                  *
+ *   - Pass a pointer to a void function taking no   *
+ *     parameters. This will cause Fake6502 to call  *
+ *     that function once after each emulated        *
+ *     instruction.                                  *
+ *                                                   *
+ *****************************************************
+ * Useful variables in this emulator:                *
+ *                                                   *
+ * uint32_t clockticks6502                           *
+ *   - A running total of the emulated cycle count.  *
+ *                                                   *
+ * uint32_t instructions                             *
+ *   - A running total of the total emulated         *
+ *     instruction count. This is not related to     *
+ *     clock cycle timing.                           *
+ *                                                   *
+ *****************************************************/
+
+/*
+ CMOS Instructions by Jonathan Foucher
+*/
+
+
 
 #ifndef cpu_h
 #define cpu_h
